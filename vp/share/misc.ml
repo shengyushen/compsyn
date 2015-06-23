ecc2d00bb4a94169d56f73b1a6f174f586747138
shengyushen <shengyushen@gmail.com>
2015-06-23 20:48:10 +0800

    adding new dir for sharing files

---
+++ vp/share/misc.ml
@@ -0,0 +1,48 @@
+open List
+
+let starttime=ref (Unix.gettimeofday ());;
+let oldtime=ref (Unix.gettimeofday ());;
+
+let set_current_time = begin
+	starttime := Unix.gettimeofday ();
+	oldtime := Unix.gettimeofday ()
+end
+and	dbg_print str = begin
+	let current_time=Unix.gettimeofday () in begin
+		Printf.printf "dbg_print : %s  time %5f delta %5f\n" str (current_time -. (!starttime)) (current_time -. (!oldtime)) ;
+		flush stdout;
+		oldtime := current_time
+	end
+end
+and getLines filename = begin
+	let file_c = open_in filename
+	in
+	let rec getLines_internal  i = begin
+		let (newln,tf)= begin
+			try 
+				let ln=input_line file_c
+				in
+				(ln,true)
+			with End_of_file ->
+				("",false)
+		end
+		in begin
+			if(tf) then 
+				newln::(getLines_internal i)
+			else []
+		end
+	end
+	in
+	let lines=getLines_internal 0
+	in 
+	begin
+		close_in file_c;
+		lines
+	end
+end
+and int2bool i = begin
+	match i with
+	0 -> false
+	|1 -> true
+	|_ -> assert false
+end
