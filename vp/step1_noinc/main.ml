002c526f7a667913b8662d68a4b5e9121736b280
shengyushen <shengyushen@icloud.com>
2015-12-22 16:54:30 +0800

    adding arg handling

--- vp/step1_noinc/main.ml
+++ vp/step1_noinc/main.ml
@@ -1,72 +1,72 @@
-(*
-usage : vp_step1 -S <verilog file> <options>
-options : 
--I <include dir>
-*)
+open Arg
+open Printf
+
 let start_time = Unix.gettimeofday ();;
 
-(*to find all include path*)
-let optlen=Array.length Sys.argv;;
-
-if optlen < 3 then begin
-	Printf.fprintf stderr "Usage 1 : vp_step1 -S <verilog file> <options> > <dst file>\n";
-	Printf.fprintf stderr "options : \n";
-	Printf.fprintf stderr "-I <include dir>\n";
-	exit 1
-end
-;;
-
-let pathlist = 
-if optlen > 3 then begin
-	let rec find_include_path i = begin
-		if i>=optlen then []
+(*
+actually my syntax is 
+many -I that can only be add to pathlist by a function
+one anonymous filename that can only be added by a function
+while those non-anonymous options with only instance can use Set_* to set
+*)
+let get_args () = begin
+	let usage_msg = "Usage : vp_step1 -S [verilog file] -I [include dir1]  -I [include dir2] > [output file]"
+	and pathlist =  ref []
+	and inputFilename = ref ""
+	in
+	let addpath str = begin
+		pathlist := str::(!pathlist)
+	end
+	and addinput str = begin
+		if((!inputFilename)="") then 
+			inputFilename := str
 		else begin
-			let pn = Sys.argv.(i)
-			in
-			if pn="-I" then (Sys.argv.(i+1))::(find_include_path (i+2))
-			else (find_include_path (i+2))
+			fprintf stderr "FATAL : more than 1 input file %s\n" str;
+			flush stderr;
+			exit 1;
 		end
 	end
+	and anon_fun str = begin
+		fprintf stderr "%s\n" usage_msg;
+		fprintf stderr "WARNING : extra option %s\n" str;
+		flush stderr;
+		exit 1
+	end
 	in
-	find_include_path 3
+	let oldspeclist = [
+	("-I",String(addpath) ," include paths");
+	("-S",String(addinput)," source file");
+	]
+	in 
+	let speclist = align oldspeclist 
+	in
+	begin
+		parse speclist anon_fun usage_msg;
+		fprintf stderr "inputFilename is %s\n" !inputFilename;
+		List.iter (fprintf stderr "path %s\n") !pathlist;
+		flush stderr;
+		(!inputFilename , !pathlist)
+	end
 end
-else []
-;;
-
+in
+let (inputFilename ,  pathlist) = get_args ()
+in  
 (*now open the file to be processed*)
-
-let inputFileName = 
-if (Sys.argv.(1)) = "-S" then
-	Sys.argv.(2)
-else begin
-	Printf.fprintf stderr "%s" Sys.argv.(2);
-	Printf.fprintf stderr "Usage 2 : vp_step1 -S <verilog file> <options> > > <dst file>\n";
-	Printf.fprintf stderr "options : \n";
-	Printf.fprintf stderr "-I <include dir>\n";
-	exit 1
+let inputFileChannle = open_in inputFilename in
+let lexbuf = Lexing.from_channel inputFileChannle in 
+begin
+	(*setting the file name in lexbuf*)
+	lexbuf.Lexing.lex_curr_p <- { lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = inputFilename };
+	(*the loop to run though source code*)
+	while true do
+		if (Proc_inc.proc_inc pathlist lexbuf)==Proc_inc.Eof then begin
+			let end_time = Unix.gettimeofday ();
+			in
+			Printf.fprintf stderr "TIME : end %f \n" (end_time -. start_time)
+			;
+			close_in inputFileChannle;
+			exit 0
+		end
+	done
 end
 
-let inputFileChannle = open_in inputFileName;;
-
-let lexbuf = Lexing.from_channel inputFileChannle;;
-
-lexbuf.Lexing.lex_curr_p <- { lexbuf.Lexing.lex_curr_p with pos_fname = inputFileName };;
-
-let parseverilog lexbuf1 = 
-	while true do
-		if (Proc_inc.proc_inc pathlist lexbuf1)==Proc_inc.Eof then  raise (Proc_inc.Ssyeof "Ssyeof") ;
-		flush stdout;
-	done
-;;
-
-try
-	parseverilog lexbuf
-with Proc_inc.Ssyeof s -> 
-		let end_time = Unix.gettimeofday ();
-		in
-		Printf.fprintf stderr "TIME : end %f \n" (end_time -. start_time)
-		;
-		exit 0
-;;
-
-close_in inputFileChannle;;
